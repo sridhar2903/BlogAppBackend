7e9a31769b028e7235ff3cb8335cf010eda935b4
sridhar2903 <saisridhard@gmail.com>
2022-04-18 18:39:41 +0530

    fitness

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const homeRouter = require("./home");
 const tourismRouter=require("./tourism")
+const fitnessRouter=require("./fitness")
 const mainrouter = express.Router();
 
 mainrouter.use("/home", homeRouter);
 mainrouter.use("/tourism", tourismRouter);
+mainrouter.use("/fitness", fitnessRouter);
 
 
 module.exports = mainrouter;
