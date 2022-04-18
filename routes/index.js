339f0b8789507062dacf6433cc5d0a844b40a76d
sridhar2903 <saisridhard@gmail.com>
2022-04-18 17:51:22 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const homeRouter = require("./home");
+const tourismRouter=require("./tourism")
 const mainrouter = express.Router();
 
 mainrouter.use("/home", homeRouter);
+mainrouter.use("/tourism", tourismRouter);
 
 
 module.exports = mainrouter;
